f576cfb66d50bdd108eea0aa1eeddcb7f1918725
VladimirStepankin <vladimirstepankin@mai.ru>
2024-01-18 12:38:08 +0300

    исправление замечаний

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,45 +2,47 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final UserDao daoJDBC = new UserDaoJDBCImpl();
-    private final UserDao daoHibernate = new UserDaoHibernateImpl();
+
+    // Выбор способа реализации DAO
+//    private final UserDao userDao = new UserDaoJDBCImpl();
+    private final UserDao userDao = new UserDaoHibernateImpl();
 
     public UserServiceImpl() {
+
     }
 
     @Override
     public void createUsersTable() {
-        daoHibernate.createUsersTable();
+        userDao.createUsersTable();
     }
 
     @Override
     public void dropUsersTable() {
-        daoHibernate.dropUsersTable();
+        userDao.dropUsersTable();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        daoHibernate.saveUser(name, lastName, age);
+        userDao.saveUser(name, lastName, age);
     }
 
     @Override
     public void removeUserById(long id) {
-        daoHibernate.removeUserById(id);
+        userDao.removeUserById(id);
     }
 
     @Override
     public List<User> getAllUsers() {
-        return daoHibernate.getAllUsers();
+        return userDao.getAllUsers();
     }
 
     @Override
     public void cleanUsersTable() {
-        daoHibernate.cleanUsersTable();
+        userDao.cleanUsersTable();
     }
 }
